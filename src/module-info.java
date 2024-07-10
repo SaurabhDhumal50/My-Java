3852ae8f694f671e0f89eb31bb8228b0c9fbf4e1
Vallabh2012 <Vallabh2012@vallabh>
2024-07-10 18:38:09 +0530

    Initial Commit

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module myfristjavaprogram {
+}
---
+++ src/myfristjavaprogram/fristprogram.java
@@ -0,0 +1,10 @@
+package myfristjavaprogram;
+
+public class fristprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Saurabh Dhumal");
+
+	}
+
+}
